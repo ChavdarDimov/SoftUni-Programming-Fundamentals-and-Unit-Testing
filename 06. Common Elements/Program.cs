0e5e3e2dfa9f4895d9c0ceb2507c7bb002cfda71
Chavdar Dimov <chavdar.d.dimov@gmail.com>
2023-10-17 14:14:14 +0300

    New tasks added.

--- 06. Common Elements/Program.cs
+++ 06. Common Elements/Program.cs
@@ -3,20 +3,20 @@
 int[] arrayOne = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
 int[] arrayTwo = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
 
-for (int index1 = 0;  index1 < arrayTwo.Length; index1++)
-{
-    for (int index2 = 0; index2 < arrayOne.Length; index2++)
-    {
-        if (arrayTwo[index1] == arrayOne[index2])
-        {
-            Console.Write($"{arrayTwo[index1]} ");
-        }
-    }
-}
-
-//var intersect = arrayOne.Intersect(arrayTwo);
-
-//foreach (int i in intersect)
+//for (int index1 = 0;  index1 < arrayTwo.Length; index1++)
 //{
-//    Console.Write(i + " ");
+//    for (int index2 = 0; index2 < arrayOne.Length; index2++)
+//    {
+//        if (arrayTwo[index1] == arrayOne[index2])
+//        {
+//            Console.Write($"{arrayTwo[index1]} ");
+//        }
+//    }
 //}
+
+var intersect = arrayOne.Intersect(arrayTwo);
+
+foreach (int i in intersect)
+{
+    Console.Write(i + " ");
+}
